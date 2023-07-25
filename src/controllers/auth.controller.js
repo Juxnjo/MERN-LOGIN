028b4bd4ba51453f9363705d6840d51b91a42494
Juxnjo <ramirezjuanj0117@gmail.com>
2023-07-25 10:25:15 -0500

    password hash

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.models.js"
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body
@@ -7,14 +8,20 @@ export const register = async (req, res) => {
     res.send("Registrando") */
 
     try {
+        const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash
         })
 
         const userSaved = await newUser.save()
-        return res.status(201).json(userSaved)
+        return res.status(201).json({
+            id: userSaved._id,
+            username: userSaved.username,
+            email: userSaved.email,
+            
+        })
 
     } catch (error) {
         res.status(500).json({ message: error.message })
