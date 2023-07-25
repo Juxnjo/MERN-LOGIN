2092623bc83af906721993dfddd5018339492e25
Juxnjo <ramirezjuanj0117@gmail.com>
2023-07-25 09:49:19 -0500

    post register ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,24 @@
 import User from "../models/user.models.js"
 
-export const register = (req, res) => res.send("register")
+export const register = async (req, res) => {
+    const {email, password, username} = req.body
+
+    /* console.log(email, password, username)
+    res.send("Registrando") */
+
+    try {
+        const newUser = new User({
+            username,
+            email,
+            password
+        })
+
+        const userSaved = await newUser.save()
+        return res.status(201).json(userSaved)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
 export const login = (req, res) => res.send("login")
