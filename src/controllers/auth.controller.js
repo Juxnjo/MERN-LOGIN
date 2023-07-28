7b90fdee9294645d5c5819e1bd2be76ec6006141
“Juan <ramirezjuanj0117@gmail.com>
2023-07-28 12:07:58 -0500

    login ok

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -30,4 +30,28 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => res.send("login")
+export const login = async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        const userFound = await User.findOne({email})
+        if(!userFound) return res.status(400).json({message:"User not found"})
+        
+        const isMatch = await bcrypt.compare(password, userFound.password)
+        if (!isMatch)
+        return res.status(400).json({message: "Error in Credentials"})
+
+        const token = await createTokenAccess({  id: userFound._id  })
+        res.cookie("token", token)
+        res.status(201).json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email
+        })
+
+    } catch (error) {
+        res.status(500).json9({ message: error.message })
+        
+    }
+
+}
