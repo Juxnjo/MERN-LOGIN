2eb12bc0201153c0560c6e07ee9d5cf3e0a4b210
“Juan <ramirezjuanj0117@gmail.com>
2023-08-01 12:08:57 -0500

    taskcontroller fixes

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     password : {
         type: String,
         required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     }
 },{
         timestamps: true
