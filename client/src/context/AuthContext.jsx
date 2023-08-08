1f47205b63a0b7305c177a48e72c825bfda83c07
“Juan <ramirezjuanj0117@gmail.com>
2023-08-08 11:39:56 -0500

    context fix

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -4,10 +4,45 @@ import { registerRequest } from "../api/auth";
 
 export const AuthContext = createContext()
 
-export const useAuth = () => {
-    const context = useContext(AuthContext)
-    if(!context){
-        throw new Error ('useAuth must be used within an AuthProvider')
+export const useAuth = () =>
+{
+    const context = useContext( AuthContext )
+    if ( !context )
+    {
+        throw new Error( 'useAuth must be used within an AuthProvider' )
     }
     return context
+}
+
+export const AuthProvider = ( { children } ) =>
+{
+    const [ user, setUser ] = useState( null )
+    const [ isAuthenticathed, setIsAuthenticathed ] = useState( false )
+    const [ errors, setErrors ] = useState( [] )
+
+    const signup = async ( user ) =>
+    {
+        try
+        {
+            const res = await registerRequest( user )
+            console.log( res )
+            setUser( res.data )
+            setIsAuthenticathed( true )
+        } catch ( error )
+        {
+            setErrors( error.response.data )
+            console.log( error )
+        }
+    }
+
+    return (
+        <AuthContext.Provider value={ {
+            signup,
+            user,
+            isAuthenticathed,
+            errors
+        } }>
+            { children }
+        </AuthContext.Provider>
+    )
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,17 +1,23 @@
 import { useForm } from 'react-hook-form'
-import { registerRequest } from '../api/auth'
+import { useAuth } from '../context/AuthContext'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function RegisterPage ()
 {
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { signup, isAuthenticated, errors: RegisterErrors } = useAuth()
+    const navigate = useNavigate()
 
+    useEffect( () =>
+    {
+        if ( isAuthenticated ) navigate( '/tasks' )
+    }, [ isAuthenticated, navigate ] )
     return (
         <div className='bg-red-900 max-w-md p-10 rounded-md'>
             <form onSubmit={ handleSubmit( async ( values ) =>
             {
-                console.log( values )
-                const res = await registerRequest( values )
-                console.log( res )
+                signup( values )
             } ) }>
                 <input type="text" { ...register( 'username', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Username' />
 
