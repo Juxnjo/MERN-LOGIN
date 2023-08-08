804b5f07b0575f61d98645950e0d62c419d36c04
“Juan <ramirezjuanj0117@gmail.com>
2023-08-08 12:58:49 -0500

    OK

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,35 @@
-function LoginPage() {
+import { useForm } from "react-hook-form"
+
+function LoginPage ()
+{
+
+  const { register, handleSubmit, formState: { errors } } = useForm()
+
+  const onSubmited = handleSubmit( async ( data ) =>
+  {
+    console.log( data )
+  } )
+
   return (
-    <div>LoginPage JSX</div>
+    <div className='bg-red-900 max-w-md p-10 rounded-md'>
+      <form onSubmit={ onSubmited } >
+
+        <input type="email" { ...register( 'email', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Email' />
+
+        {
+          errors.email && <p className='text-red-500'>Email is Required</p>
+        }
+
+
+        <input type="password" { ...register( 'password', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Password' />
+
+        {
+          errors.password && <p className='text-red-500'>Password is Required</p>
+        }
+
+        <button type="submit" className="w-full bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-md my-2">Register</button>
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,30 +1,51 @@
-import { useForm } from 'react-hook-form'
-import { useAuth } from '../context/AuthContext'
-import { useEffect } from 'react'
+
+import { useForm } from 'react-hook-form';
+import { useAuth } from '../context/authContext';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 function RegisterPage ()
 {
-    const { register, handleSubmit, formState: { errors } } = useForm()
-    const { signup, isAuthenticated, errors: RegisterErrors } = useAuth()
-    const navigate = useNavigate()
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { signup, isAuthenticathed, errors: RegisterErrors } = useAuth();
+    const navigate = useNavigate();
 
     useEffect( () =>
     {
-        if ( isAuthenticated ) navigate( '/tasks' )
-    }, [ isAuthenticated, navigate ] )
+        if ( isAuthenticathed ) navigate( '/tasks' );
+    }, [ isAuthenticathed, navigate ] )
+
+    const onSubmited = handleSubmit( async ( values ) =>
+    {
+        signup( values )
+    } )
+
+
+
     return (
         <div className='bg-red-900 max-w-md p-10 rounded-md'>
-            <form onSubmit={ handleSubmit( async ( values ) =>
+
             {
-                signup( values )
-            } ) }>
-                <input type="text" { ...register( 'username', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Username' />
+                RegisterErrors.map( ( error, i ) => (
+                    <div className='bg-red-500 text-white p-2' key={ i }>
+                        { error }
+                    </div>
+                ) )
+            }
 
-                <input type="email" { ...register( 'email', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Email' />
-
-                <input type="password" { ...register( 'password', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Password' />
-
+            <form onSubmit={ onSubmited }>
+                <input type="text" { ...register( "username", { required: true } ) } placeholder='UserName' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
+                {
+                    errors.username && <p className='text-red-500'>UserName is required</p>
+                }
+                <input type="email" { ...register( "email", { required: true } ) } placeholder='email' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
+                {
+                    errors.email && <p className='text-red-500'>Email is required</p>
+                }
+                <input type="password" { ...register( "password", { required: true } ) } placeholder='password' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
+                {
+                    errors.password && <p className='text-red-500'>Password is required</p>
+                }
                 <button type="submit" className="w-full bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-md my-2">Register</button>
             </form>
         </div>
