27f7affd52450a8d37545a5dc4344d204cbc8df7
“Juan <ramirezjuanj0117@gmail.com>
2023-08-25 00:19:50 -0500

    fox pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -11,24 +11,26 @@ function LoginPage ()
   } )
 
   return (
-    <div className='bg-red-900 max-w-md p-10 rounded-md'>
-      <form onSubmit={ onSubmited } >
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className='bg-red-900 max-w-md p-10 rounded-md'>
+        <form onSubmit={ onSubmited } >
 
-        <input type="email" { ...register( 'email', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Email' />
+          <input type="email" { ...register( 'email', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Email' />
 
-        {
-          errors.email && <p className='text-red-500'>Email is Required</p>
-        }
+          {
+            errors.email && <p className='text-red-500'>Email is Required</p>
+          }
 
 
-        <input type="password" { ...register( 'password', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Password' />
+          <input type="password" { ...register( 'password', { required: true } ) } className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' placeholder='Password' />
 
-        {
-          errors.password && <p className='text-red-500'>Password is Required</p>
-        }
+          {
+            errors.password && <p className='text-red-500'>Password is Required</p>
+          }
 
-        <button type="submit" className="w-full bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-md my-2">Register</button>
-      </form>
+          <button type="submit" className="w-full bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-md my-2">Register</button>
+        </form>
+      </div>
     </div>
   )
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -23,32 +23,35 @@ function RegisterPage ()
 
 
     return (
-        <div className='bg-red-900 max-w-md p-10 rounded-md'>
+        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+            <div className='bg-red-900 max-w-md p-10 rounded-md'>
 
-            {
-                RegisterErrors.map( ( error, i ) => (
-                    <div className='bg-red-500 text-white p-2' key={ i }>
-                        { error }
-                    </div>
-                ) )
-            }
+                {
+                    RegisterErrors.map( ( error, i ) => (
+                        <div className='bg-red-500 text-white p-2' key={ i }>
+                            { error }
+                        </div>
+                    ) )
+                }
 
-            <form onSubmit={ onSubmited }>
-                <input type="text" { ...register( "username", { required: true } ) } placeholder='UserName' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
-                {
-                    errors.username && <p className='text-red-500'>UserName is required</p>
-                }
-                <input type="email" { ...register( "email", { required: true } ) } placeholder='email' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
-                {
-                    errors.email && <p className='text-red-500'>Email is required</p>
-                }
-                <input type="password" { ...register( "password", { required: true } ) } placeholder='password' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
-                {
-                    errors.password && <p className='text-red-500'>Password is required</p>
-                }
-                <button type="submit" className="w-full bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-md my-2">Register</button>
-            </form>
+                <form onSubmit={ onSubmited }>
+                    <input type="text" { ...register( "username", { required: true } ) } placeholder='UserName' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
+                    {
+                        errors.username && <p className='text-red-500'>UserName is required</p>
+                    }
+                    <input type="email" { ...register( "email", { required: true } ) } placeholder='email' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
+                    {
+                        errors.email && <p className='text-red-500'>Email is required</p>
+                    }
+                    <input type="password" { ...register( "password", { required: true } ) } placeholder='password' className='w-full bg-red-700 text-white px-4 py-2 rounded-md my-2' />
+                    {
+                        errors.password && <p className='text-red-500'>Password is required</p>
+                    }
+                    <button type="submit" className="w-full bg-red-600 hover:bg-red-400 text-white px-4 py-2 rounded-md my-2">Register</button>
+                </form>
+            </div>
         </div>
+
     )
 }
 
