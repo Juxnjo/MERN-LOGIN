e6bc1c91d7d954238a837e596b15e31c3affd8f3
“Juan <ramirezjuanj0117@gmail.com>
2023-08-25 00:57:42 -0500

    signIn fix

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -48,6 +48,11 @@ export const AuthProvider = ( { children } ) =>
         } catch ( error )
         {
             console.log( error )
+            if ( Array.isArray( error.response.data ) )
+            {
+                setErrors( error.response.data )
+            }
+            setErrors( [ error.response.data.message ] )
 
         }
     }
