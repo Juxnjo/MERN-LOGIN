7790f5e4435cd382bf1e5a37adb59e38b7b06bd6
“Juan <ramirezjuanj0117@gmail.com>
2023-08-29 14:26:05 -0500

    setErrors

--- client/src/ProtectedRoute.jsx
+++ client/src/ProtectedRoute.jsx
@@ -1,12 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 
 const ProtectedRoute = () =>
 {
     const { user ,isAuthenticated } = useAuth();
     console.log(user, isAuthenticated);
-    
+    if (!isAuthenticated) return <Navigate to='/login' replace />
+
     return (
-        <div>ProtectedRoute</div>
+        <div>
+            <Outlet/>
+        </div>
     )
 }
 
--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 import { loginRequest, registerRequest } from "../api/auth";
 
@@ -57,6 +57,14 @@ export const AuthProvider = ( { children } ) =>
         }
     }
 
+    useEffect(() => {
+        if(errors.length > 0){
+            const timer = setTimeout(() => {
+                setErrors([])
+            }, 5000);
+            return () => clearTimeout(timer);
+        }
+    }, [errors])
 
 
     return (
