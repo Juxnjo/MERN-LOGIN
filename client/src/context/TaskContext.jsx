b804571e353e48be1590d4a7020dc613e2f9a2e1
“Juan <ramirezjuanj0117@gmail.com>
2023-09-08 00:51:04 -0500

    getTasks page

--- client/src/context/TaskContext.jsx
+++ client/src/context/TaskContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { createTaskRequest } from "../api/task";
+import { createTaskRequest, getTasksRequest } from "../api/task";
 
 const TaskContext = createContext()
 
@@ -15,9 +15,20 @@ export const useTasks = () =>
     return context
 }
 
+// eslint-disable-next-line react/prop-types
 export function TaskProvider ( { children } )
 {
     const [ task, setTasks ] = useState( [] )
+
+    const getTasks = async () => {
+        try {
+            const res = await getTasksRequest()
+            setTasks( res.data)
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const createTask = async (task) => {
         console.log('task created')
@@ -30,7 +41,8 @@ export function TaskProvider ( { children } )
     return (
         <TaskContext.Provider value={ {
             task,
-            createTask
+            createTask,
+            getTasks
 
         } }>
             { children }
--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useTasks } from "../context/AuthContext";
+
 const TaskPage = () => {
+  
+  const { task, getTasks } = useTasks()
+  console.log(task)
+
+  useEffect
+
   return (
     <div>TaskPage</div>
   )
